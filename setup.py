44e1c6c1c2ab8eefc065d9d50329d38d3e4ed0c0
Paul Swartz <paulswartz@gmail.com>
2012-04-17 14:27:23 -0400

    initial config

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MiroCommunity-SAAS',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Participatory Culture Foundation',
+      author_email='pswartz@pculture.org',
+      url='http://www.mirocommunity.org/',
+      packages=['mirocommunity_saas'])
+
