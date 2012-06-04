5517fa173185963423a08dad74c7bb1ba65001e7
Stephen Burrows <stephen.r.burrows@gmail.com>
2012-06-03 21:40:06 -0700

    Made sure that signals would be imported (and before models.)

--- mirocommunity_saas/__init__.py
+++ mirocommunity_saas/__init__.py
@@ -15,3 +15,5 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Miro Community.  If not, see <http://www.gnu.org/licenses/>.
 
+# Trigger tiers signal registration.
+from mirocommunity_saas import utils
