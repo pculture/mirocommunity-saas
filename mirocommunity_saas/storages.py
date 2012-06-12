ea057979de4dcea83f5e144f9c275d925b541c57
Stephen Burrows <stephen.r.burrows@gmail.com>
2012-06-12 15:05:38 -0700

    Added some storages which modify locations for static and compressed files.

--- mirocommunity_saas/storages.py
+++ mirocommunity_saas/storages.py
@@ -1,6 +1,7 @@
 import random
 
 from boto.s3.connection import OrdinaryCallingFormat
+from storages.backends.s3boto import S3BotoStorage
 
 
 class MultiCallingFormat(OrdinaryCallingFormat):
@@ -16,3 +17,24 @@ class MultiCallingFormat(OrdinaryCallingFormat):
         format = random.choice(self.formats)
         return format.build_url_base(connection, protocol, server, bucket,
                                      key)
+
+
+class StaticBotoStorage(S3BotoStorage):
+    """
+    By default, uses 'static' as the location for this storage's instances.
+
+    """
+    def __init__(self, **kwargs):
+        if 'location' not in kwargs:
+            kwargs['location'] = 'static'
+        super(StaticBotoStorage, self).__init__(**kwargs)
+
+
+class CompressedBotoStorage(S3BotoStorage):
+    """
+    Adds 'compressed' to the location for this storage's instances.
+
+    """
+    def __init__(self, *args, **kwargs):
+        super(StaticBotoStorage, self).__init__(*args, **kwargs)
+        self.location = '/'.join((self.location, 'compressed'))
