946d55462c59ff8aee14a825880ec37721b79a11
Stephen Burrows <stephen.r.burrows@gmail.com>
2012-12-20 03:11:45 -0800

    Corrected some tests/behavior for uploadtemplate changes.

--- mirocommunity_saas/tests/__init__.py
+++ mirocommunity_saas/tests/__init__.py
@@ -86,11 +86,10 @@ class BaseTestCase(MCBaseTestCase):
 
     def create_theme(self, name='Test', site_id=settings.SITE_ID,
                      description='Test description', default=False, **kwargs):
-        theme = Theme.objects.create(name=name, site_id=site_id,
-                                     description=description, **kwargs)
         if default:
-            Theme.objects.set_default(theme)
-        return theme
+            Theme.objects.filter(site_id=site_id, default=True).update(default=False)
+        return Theme.objects.create(name=name, site_id=site_id,
+                                    description=description, **kwargs)
 
     def create_ipn(self, ipaddress="", **kwargs):
         return PayPalIPN.objects.create(ipaddress=ipaddress, **kwargs)
