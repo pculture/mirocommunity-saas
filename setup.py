b15cccebff172e6ce971aca671870af70593f764
Stephen Burrows <stephen.r.burrows@gmail.com>
2013-01-23 16:10:56 -0800

    Altered setup/version to better match mc core.

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-#!/usr/bin/env python
-
 from setuptools import setup, find_packages
 
-setup(name='MiroCommunity-SAAS',
-      version='1.9',
-      description="Public Miro Community files specific to the service PCF "
-      			  "provides.",
+__version__ = __import__('mirocommunity_saas').__version__
+
+description = ("Public Miro Community files specific to the service PCF "
+               "provides.")
+
+setup(name='mirocommunity-saas',
+      version='.'.join([str(v) for v in __version__]),
+      description=description,
+      long_description=description,
       author='Participatory Culture Foundation',
       author_email='dev@mirocommunity.org',
       url='http://www.mirocommunity.org/',
@@ -16,6 +19,7 @@ setup(name='MiroCommunity-SAAS',
           'Framework :: Django',
           'Intended Audience :: Developers',
           'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: GNU Affero General Public License v3',
           'Operating System :: OS Independent',
           'Programming Language :: Python',
           'Topic :: Multimedia :: Sound/Audio',
